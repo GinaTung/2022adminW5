f5685c7b435e42e04d56c0e57d14175078cc6cf6
董聿凌 <gb2013429@gmail.com>
2022-08-17 00:16:28 +0800

    Update 2022-08-16T16:16:24.863Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -3,4 +3,11 @@
 $(function () {
   console.log('Hello Bootstrap5');
 });
+"use strict";
+
+ClassicEditor.create(document.querySelector('#editor')).then(function (editor) {
+  console.log(editor);
+})["catch"](function (error) {
+  console.error(error);
+});
 //# sourceMappingURL=all.js.map
