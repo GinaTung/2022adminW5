c1fabd17bb4785cab10150a4fcab02779a682ae2
董聿凌 <gb2013429@gmail.com>
2022-08-17 19:41:06 +0800

    Update 2022-08-17T11:41:03.029Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,11 +1,23 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
-});
+// $(function() {
+//   console.log('Hello Bootstrap5');
+// });
 ClassicEditor.create(document.querySelector('#editor')).then(function (editor) {
   console.log(editor);
 })["catch"](function (error) {
   console.error(error);
+});
+$(function () {
+  $('.admin-reply').click(function () {
+    $('.card-admin').addClass('d-none');
+    $('.admin-btn').addClass('d-none');
+    $('.admin-collapse').show();
+  });
+  $('.edit-cancel').click(function () {
+    $('.card-admin').removeClass('d-none');
+    $('.admin-btn').removeClass('d-none');
+    $('.admin-collapse').hide();
+  });
 });
 //# sourceMappingURL=all.js.map
