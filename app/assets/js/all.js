8de413a6cbcea6141a0199067f9f1854b9690246
董聿凌 <gb2013429@gmail.com>
2022-08-17 19:40:33 +0800

    collapse

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,6 +1,6 @@
-$(function() {
-  console.log('Hello Bootstrap5');
-});
+// $(function() {
+//   console.log('Hello Bootstrap5');
+// });
 ClassicEditor
   .create( document.querySelector( '#editor' ) )
   .then( editor => {
@@ -9,3 +9,16 @@ ClassicEditor
   .catch( error => {
   console.error( error );
 } );
+
+$(function() {
+  $('.admin-reply').click(function () {
+    $('.card-admin').addClass('d-none');
+    $(('.admin-btn')).addClass('d-none');
+    $(('.admin-collapse')).show();
+  })
+  $('.edit-cancel').click(function () {
+    $('.card-admin').removeClass('d-none');
+    $(('.admin-btn')).removeClass('d-none');
+    $(('.admin-collapse')).hide();
+  })
+});
